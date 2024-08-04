18a8f6e0270baca417785def02700254392aac74
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2024-08-04 18:47:14 +0530

    Delete 2_Basic CRUD (Create, Read, Update, Delete) directory

--- 2_Basic CRUD (Create, Read, Update, Delete)/configre.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost:3308", "root", "", "user_db");
-
-if(!$conn){
-    die("Error DB CONNECT". mysqli_connect_error());
-}else{
-    //echo "Connection Success";
-}
-?>
